57f5a26cbd40508f0fd5d8474ddba49ced073bd0
Maxis Kao <maxiskao@yahoo-inc.com>
2015-05-09 23:31:46 +0800

    Add README and gitignore list

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express'),
-    app = express(),
-    _ = require('lodash')._,
-    cons = require('consolidate');
+    app = express();
 
 // Register '.tpl' extension with lodash templating
-app.engine('tpl', cons.lodash);
-
+require('lodash-express')(app, 'tpl');
 app.set('view engine', 'tpl');
+
+// set the template root to /templates (default: /views)
 app.set('views', __dirname + '/templates');
 
 app.get('/', function (req, res) {
@@ -20,8 +19,6 @@ var data = {
     }
 };
 
-console.log(data.simple);
-
 app.get('/simple', function (req, res) {
     res.render('simple', data.simple );
 });
@@ -29,6 +26,7 @@ app.get('/simple', function (req, res) {
 app.get('/greeting', function (req, res) {
     res.render('index', req.query)
 });
+
 
 var server = app.listen(3000, function() {
     console.log('listening at localhost:3000');
--- templates/index.tpl
+++ templates/index.tpl
@@ -1,3 +1,5 @@
 
+<%= include('header') %>
 <h1>this is index</h1>
 <h2>my name is <%= name %> </h2>
+<%= include('footer') %>
