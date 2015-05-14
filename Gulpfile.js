d46a4cc8b7c3e7d5d8c5378e404bbb777ac4bf44
Maxis <maxis1718@gmail.com>
2015-05-15 00:48:19 +0800

    Refactor gulp task by separating styles and scripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,26 +1,52 @@
-var gulp = require('gulp'),
-    compass = require('gulp-compass'),
-    browserSync = require('browser-sync'),
-    reload = browserSync.reload,
-    nodemon = require('gulp-nodemon');
+var gulp            = require('gulp'),
+    compass         = require('gulp-compass'),
+    autoprefixer    = require('gulp-autoprefixer'),
+    concat          = require('gulp-concat'),
+    browserSync     = require('browser-sync'),
+    reload          = browserSync.reload,
+    path            = require('path'),
+    nodemon         = require('gulp-nodemon');
 
-var sassSrc = './src/styles/sass/*.sass',
-    sassDir = './src/styles/sass';
+var paths = {
+    sass: {
+        src: 'src/styles/sass/*.sass',
+        dir: 'src/styles/sass',
+        dest: 'public/css'
+    },
+    js: {
+        src: 'src/js/*.js',
+        dest: 'public/js'
+    }
+};
 
-gulp.task('default', function () {
+gulp.task('default', ['styles', 'scripts'], function () {
     console.log('Hi Here is gulp default');
 });
 
-gulp.task('compass',function(){
-    gulp.src(sassSrc)
-	.pipe(compass({
-	    css: './public/css',
-	    sass: sassDir
-        }))
+gulp.task('styles', function () {
+    return gulp.src(paths.sass.src)
+            .pipe(compass({
+                css: paths.sass.dest,
+                sass: paths.sass.dir
+            }))
+            .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+            .pipe(gulp.dest(paths.sass.dest));
+            // uncomment the following lines if minified css is need
+            // and make sure have gulp-minify-css installed
+            // .pipe(rename({ suffix: '.min' }))
+            // .pipe(minifycss())
+            // .pipe(gulp.dest(path.js.dest));
 });
 
-gulp.task('watch', function () {
-  gulp.watch(sassSrc, ['compass']); 
+gulp.task('scripts', function () {
+    return gulp.src(paths.js.src)
+            .pipe(concat('main.js'))
+            .pipe(gulp.dest(paths.js.dest))
+            // uncomment the following lines if minified js is needed
+            // and make sure have gulp-uglify installed 
+            // .pipe(rename({ suffix: '.min' }))
+            // .pipe(uglify())
+            // .pipe(gulp.dest(paths.js.dest));
 });
 
 gulp.task('nodemon', function (cb) {
@@ -35,15 +61,25 @@ gulp.task('nodemon', function (cb) {
     });
 });
 
-gulp.task('serve', ['default', 'nodemon', 'watch'], function() {
+gulp.task('serve', ['default', 'nodemon'], function() {
+
+    // listen on 7000, which is a proxy of express server running on 3000
     browserSync({
         proxy: "http://localhost:3000",
         port: 7000,
     });
+    
+    // watch .sass files
+    gulp.watch(paths.sass.src, ['styles']);
+
+    // watch .js files
+    gulp.watch(paths.js.src, ['scripts']);
+
+    // reload when a template file, the compiled css, or the js file changes
     gulp.watch([
         'views/**/*.tpl', 
-        'public/css/*.css', 
-        'public/js/*.js'])
+        path.join(paths.sass.dest, '*.css'),
+        path.join(paths.js.dest, '*.js')])
     .on('change', reload);
 });
 
